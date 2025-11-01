acfec2a0f9f3fb4776f44651481eef751f66794a
mkijowski <maciekkij01@gmail.com>
2025-11-01 18:06:03 +0100

    feat/front - edit quiz

--- frontend/src/features/quizzes/pages/EditQuizPage.tsx
+++ frontend/src/features/quizzes/pages/EditQuizPage.tsx
@@ -13,129 +13,208 @@ const EditQuizPage = () => {
     const { quizId } = useParams<{ quizId: string }>();
     const { data, error, isLoading } = useGetQuizData(quizId!);
 
-    const [selectedQuestion, setSelectedQuestion] = useState(0);
-    const [favorite, setFavorite] = useState(false);
-    const [question, setQuestion] = useState("");
-    const [options, setOptions] = useState([]);
+    const [allData, setAllData] = useState<Record<string, any>>({});
+    const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
 
     useEffect(() => {
-        if (data && data.questions && data.questions[selectedQuestion]) {
-            const question = data.questions[selectedQuestion];
+        if (data?.questions) {
+            const questions = data.questions;
 
-            setQuestion(question.question);
-            setOptions(
-                question.answers?.map((a, index) => ({
-                    id: a.id || uuidv4(), // ID z API lub fallback
-                    text: a.text,
-                    correct: a.correct,
-                })) || []
+            setAllData(
+                questions.reduce((acc, item) => {
+                    const uid = uuidv4();
+                    acc[uid] = item;
+                    return acc;
+                }, {})
             );
-
-            setFavorite(question.isFavorite || false);
+            const firstId = Object.keys(questions)[0];
+            setSelectedQuestion(firstId);
         }
-    }, [data, selectedQuestion]);
+    }, [data]);
 
     if (isLoading) return <p>Ładowanie...</p>;
     if (error) return <p>Błąd: {String(error)}</p>;
 
     console.log("data", data);
+    console.log("allData", allData);
 
     const handleSelectQuestion = (index) => {
         setSelectedQuestion(index);
     };
 
     const handleToggleFavorite = () => {
-        const newValue = !favorite;
-        setFavorite(newValue);
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                isFavorite: !prev[selectedQuestion].isFavorite,
+            },
+        }));
     };
 
-    const handleToggleCorrect = (id) => {
-        setOptions((prev) =>
-            prev.map((opt) => ({
-                ...opt,
-                correct: opt.id === id,
-            }))
-        );
+    const handleToggleCorrect = (number: number) => {
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                answers: prev[selectedQuestion].answers.map((opt, index) => ({
+                    ...opt,
+                    correct: index === number,
+                })),
+            },
+        }));
     };
 
-    const onQuestionChange = (text) => {
-        setQuestion(text);
+    const onQuestionChange = (text: string) => {
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                question: text,
+            },
+        }));
     };
 
-    const handleTextChange = (id, newText) => {
-        setOptions((prev) => prev.map((opt) => (opt.id === id ? { ...opt, text: newText } : opt)));
+    const handleAddQuestion = () => {
+        const uid = uuidv4();
+
+        const answers = Array.from({ length: 3 }, () => ({
+            text: "",
+            correct: false,
+        }));
+        setAllData((prev) => ({
+            ...prev,
+            [uid]: {
+                question: "",
+                answers,
+                isFavorite: false,
+            },
+        }));
     };
 
-    const handleDelete = (id) => {
-        setOptions((prev) => prev.filter((opt) => opt.id !== id));
+    const handleAddOption = () => {
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                answers: [...prev[selectedQuestion].answers, { text: "", correct: false }],
+            },
+        }));
     };
+
+    const handleTextChange = (number: string, newText: string) => {
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                answers: prev[selectedQuestion].answers.map((opt, index) =>
+                    index === number ? { ...opt, text: newText } : opt
+                ),
+            },
+        }));
+    };
+
+    const handleDelete = (id: string) => {
+        if (!selectedQuestion) return;
+        setAllData((prev) => ({
+            ...prev,
+            [selectedQuestion]: {
+                ...prev[selectedQuestion],
+                answers: prev[selectedQuestion].answers.filter((opt) => opt.id !== id),
+            },
+        }));
+    };
+    const selected = selectedQuestion ? allData[selectedQuestion] : null;
+    console.log("selected", selected);
 
     return (
         <div className="flex flex-col w-full ">
             <div className="w-full flex flex-row gap-5">
+                {/* QUESTIONS SECTION */}
                 <div className="w-1/4 bg-white border rounded-md p-5 flex flex-col max-h-screen overflow-y-scroll">
                     <div className="flex flex-row items-center justify-between  mb-5">
                         <p className="text-md font-semibold">Questions</p>
-                        <Button variant={"default"}>
+                        <Button variant={"default"} onClick={handleAddQuestion}>
                             <FaPlus />
                             Add
                         </Button>
                     </div>
                     <div className="flex flex-col gap-3">
-                        {data.questions.map((item, index) => (
-                            <QuestionTab data={item} number={index} handleSelectQuestion={handleSelectQuestion} />
+                        {Object.entries(allData).map(([id, questionData]) => (
+                            <QuestionTab
+                                key={id}
+                                data={questionData}
+                                number={id}
+                                handleSelectQuestion={handleSelectQuestion}
+                            />
                         ))}
                     </div>
                 </div>
+                {/* OPTIONS SECTION */}
                 <div className="w-3/4 bg-white border rounded-md flex flex-col p-5 gap-5">
-                    <div className="w-full flex flex-row items-center justify-between">
-                        <p className="text-xl font-semibold">Question 1</p>
-                        <div className="flex flex-row items-center gap-5">
-                            <div
-                                onClick={handleToggleFavorite}
-                                className={cn(
-                                    "h-10 w-10 rounded-full flex justify-center items-center text-gray-400 cursor-pointer",
-                                    favorite && "text-yellow-400"
-                                )}
-                            >
-                                {favorite ? <FaStar size={20} /> : <FaRegStar size={20} />}
+                    {selected && (
+                        <>
+                            <div className="w-full flex flex-row items-center justify-between">
+                                <p className="text-xl font-semibold">Question 1</p>
+                                <div className="flex flex-row items-center gap-5">
+                                    <div
+                                        onClick={handleToggleFavorite}
+                                        className={cn(
+                                            "h-10 w-10 rounded-full flex justify-center items-center text-gray-400 cursor-pointer",
+                                            selected.isFavorite && "text-yellow-400"
+                                        )}
+                                    >
+                                        {selected.isFavorite ? <FaStar size={20} /> : <FaRegStar size={20} />}
+                                    </div>
+                                    <div
+                                        onClick={handleToggleFavorite}
+                                        className={cn(
+                                            "h-10 w-10 rounded-full flex justify-center items-center text-gray-400 cursor-pointer hover:text-red-700"
+                                        )}
+                                    >
+                                        <FaTrashCan size={20} />
+                                    </div>
+                                </div>
                             </div>
-                            <div
-                                onClick={handleToggleFavorite}
-                                className={cn(
-                                    "h-10 w-10 rounded-full flex justify-center items-center text-gray-400 cursor-pointer hover:text-red-700"
-                                )}
-                            >
-                                <FaTrashCan size={20} />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="w-full flex flex-col">
-                        <p className="text-sm text-gray-700 font-semibold mb-2">Question Text</p>
-                        <textarea
-                            value={question}
-                            onChange={(e) => onQuestionChange(e.target.value)}
-                            className="border rounded-md p-3 h-30 resize-none"
-                        />
-                    </div>
-                    <div className="w-full flex flex-col">
-                        <p className="text-sm text-gray-700 font-semibold mb-2">Answer Options</p>
-                        <div className="space-y-3">
-                            {options.map((option) => (
-                                <AnswerOptionItem
-                                    key={option.id}
-                                    option={option}
-                                    onToggleCorrect={() => handleToggleCorrect(option.id)}
-                                    onTextChange={(text) => handleTextChange(option.id, text)}
-                                    onDelete={() => handleDelete(option.id)}
+                            <div className="w-full flex flex-col">
+                                <p className="text-sm text-gray-700 font-semibold mb-2">Question Text</p>
+                                <textarea
+                                    value={selected.question}
+                                    onChange={(e) => onQuestionChange(e.target.value)}
+                                    className="border rounded-md p-3 h-30 resize-none"
                                 />
-                            ))}
-                        </div>
-                    </div>
-                    <div className="w-full flex flex-col">
-                        <p className="text-sm text-gray-700 font-semibold mb-2">Explanation (Optional)</p>
-                        <textarea className="border rounded-md p-3 h-30 resize-none" />
-                    </div>
+                            </div>
+                            <div className="w-full flex flex-col">
+                                <div className="w-full flex flex-row justify-between items-center">
+                                    <p className="text-sm text-gray-700 font-semibold mb-2">Answer Options</p>
+                                    <Button variant={"defaultLine"} onClick={handleAddOption}>
+                                        <FaPlus />
+                                        Add Option
+                                    </Button>
+                                </div>
+                                <div className="space-y-3">
+                                    {selected.answers.map((option, index) => (
+                                        <AnswerOptionItem
+                                            key={option.id}
+                                            option={option}
+                                            onToggleCorrect={() => handleToggleCorrect(index)}
+                                            onTextChange={(text) => handleTextChange(index, text)}
+                                            onDelete={() => handleDelete(option.id)}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="w-full flex flex-col">
+                                <p className="text-sm text-gray-700 font-semibold mb-2">Explanation (Optional)</p>
+                                <textarea className="border rounded-md p-3 h-30 resize-none" />
+                            </div>
+                        </>
+                    )}
                 </div>
             </div>
             <div className="w-full flex flex-row justify-between items-center mt-5">
